131ec78ed69320cb2bbeffee3d2f113f15cd288b
Sarah Barron <s.barron2338@gmail.com>
2019-01-28 16:07:58 -0600

    select exercises completed

---
+++ select_exercises.sql
@@ -0,0 +1,21 @@
+USE codeup_test_db;
+
+SELECT 'The name of all albums by Pink Floyd' AS 'info';
+SELECT name FROM ALBUMS where artist = 'pink floyd';
+
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released
+' AS 'info';
+SELECT release_date from albums where name like '%sgt. pepper%';
+
+SELECT 'The genre for Nevermind' AS 'info';
+SELECT genre FROM ALBUMS where name = 'nevermind';
+
+SELECT 'Which albums were released in the 1990s' AS 'info';
+SELECT name FROM ALBUMS where release_date between 1990 and 2000;
+
+SELECT 'Which albums had less than 20 million certified sales' AS 'info';
+SELECT name FROM ALBUMS where sales < 20;
+
+SELECT 'All the albums with a genre of "Rock"' AS 'info';
+SELECT * FROM ALBUMS where genre like '%rock%';
+
