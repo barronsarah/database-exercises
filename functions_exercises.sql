20f4687566ead8528849157d66efbe4f0a56b72d
Sarah Barron <s.barron2338@gmail.com>
2019-01-30 10:18:02 -0600

    functions walkthrough

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -15,5 +15,11 @@ select DATEDIFF(NOW(), hire_date) from employees
 where hire_date between '1989-12-31' and '2000-01-01'
 and birth_date like '%-12-25';
 
-
+-- walkthrough answer OF last answer:
+select emp_no, CONCAT(first_name, ' ', last_name) as full_name,
+datediff(curdate(), hire_date) as days_employed
+from employees
+where hire_date between '1989-12-31' and '2000-01-01'
+and birth_date like '%-12-25'
+ORDER BY hire_date desc, birth_date;
 
