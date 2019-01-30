df9a618efd31dc2ffe10a4c78e07504a652cda07
Sarah Barron <s.barron2338@gmail.com>
2019-01-29 22:24:32 -0600

    alias exercise completed

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -11,6 +11,27 @@ select COUNT(DISTINCT last_name)
  from employees
 where last_name like '%q%'
 
-select COUNT(gender) from employees
+select COUNT(gender), gender
+ from employees
 where first_name in ('Irena', 'Vidya', 'Maya')
  GROUP BY gender;
+
+-- EXAMPLES FROM CURICULLUM
+-- SELECT CONCAT(first_name, ' ', last_name) AS full_name
+-- FROM employees
+-- GROUP BY full_name
+-- -- ORDER BY last_name
+-- LIMIT 25;
+
+-- SELECT hire_date, COUNT(*) as number_hired
+-- FROM employees
+-- GROUP BY hire_date
+-- ORDER BY number_hired DESC
+-- LIMIT 10;
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees AS emp
+-- GROUP BY full_name
+ORDER BY last_name
+LIMIT 25;
+
